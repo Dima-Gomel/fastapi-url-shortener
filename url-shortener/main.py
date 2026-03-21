5c661bda6abd64e8bf9f1b3fa660f08a4a2f68e6
Dzmitry Ivanou <metalland2015@yandex.ru>
2026-03-21 21:52:42 +0300

    add test_root_view

--- url-shortener/main.py
+++ url-shortener/main.py
@@ -1,11 +1,9 @@
 import logging
 
-from fastapi import (
-    FastAPI,
-    Request,
-)
+from fastapi import FastAPI
 
 from api import router as api_router
+from api.main_views import router as main_router
 from api.redirect_views import router as redirect_views
 from app_lifespan import lifespan
 from core import config
@@ -19,20 +17,7 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+
+app.include_router(main_router)
 app.include_router(redirect_views)
 app.include_router(api_router)
-
-
-@app.get("/")
-def read_root(
-    request: Request,
-    name: str = "World",
-) -> dict[str, str]:
-    docs_url = request.url.replace(
-        path="/docs",
-        query="",
-    )
-    return {
-        "message": f"Hello {name}",
-        "docs": str(docs_url),
-    }
